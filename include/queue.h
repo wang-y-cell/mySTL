f46debc32075c7880bbcf464ebd648a1a23d2c13
wang_y <3084003139@qq.com>
2026-01-23 12:43:57 +0800

    add prioirty_queue

--- include/queue.h
+++ include/queue.h
@@ -2,6 +2,10 @@
 #define MYSTL_QUEUE_H
 
 #include "deque.h"
+#include "vector.h"
+#include "stl_functional.h"
+#include "stl_heap.h"
+#include "move.h"
 
 namespace msl {
 
@@ -62,6 +66,77 @@ template <typename T, typename Sequence>
 bool operator>=(const queue<T, Sequence>& x, const queue<T, Sequence>& y) {
     return !(x < y);
 }
+
+
+template<typename T, 
+         typename Sequence = msl::vector<T>, 
+         typename Compare = msl::less<T>> 
+class priority_queue{
+public:
+    typedef typename Sequence::value_type value_type;
+    typedef typename Sequence::size_type size_type;
+    typedef typename Sequence::reference reference;
+    typedef typename Sequence::const_reference const_reference;
+
+protected:
+    Sequence c;
+    Compare comp;
+
+public:
+    explicit priority_queue(const Compare& x = Compare(), const Sequence& s = Sequence()) 
+        : c(s), comp(x) {
+        msl::make_heap(c.begin(), c.end(), comp);
+    }
+
+    template<typename InputIterator>
+    priority_queue(InputIterator first, InputIterator last, 
+                   const Compare& x = Compare())
+                : c(first,last), comp(x) {
+                msl::make_heap(c.begin(), c.end(), comp);
+    }
+
+    priority_queue(const priority_queue& x) : c(x.c), comp(x.comp) {}
+
+    #if MYSTL_CPP_VERSION >= 11
+    priority_queue(priority_queue&& x) noexcept
+        : c(msl::move(x.c)), comp(msl::move(x.comp)){ }
+
+    priority_queue& operator=(priority_queue&& x) noexcept {
+        c = msl::move(x.c);
+        comp = msl::move(x.comp);
+        return *this;
+    }
+    #endif
+
+    priority_queue& operator=(const priority_queue& x) {
+        c = x.c;
+        comp = x.comp;
+        return *this;
+    }
+
+    bool empty() const { return c.empty(); }
+    size_type size() const { return c.size(); }
+    const_reference top() const { return c.front(); }
+    
+    void push(const value_type& x) {
+        MYSTL_TRY {
+            c.push_back(x);
+            msl::push_heap(c.begin(), c.end(), comp);
+        }
+        MYSTL_UNWIND(c.clear())
+    }
+    
+    void pop() {
+        MYSTL_TRY {
+            msl::pop_heap(c.begin(), c.end(), comp);
+            c.pop_back();
+        }
+        MYSTL_UNWIND(c.clear())
+    }
+};
+
+
+
 
 } // namespace msl
 
---
+++ runner/src/priority_queue_test.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include "queue.h"
+#include <vector>
+#include <cassert>
+
+// Simple test for priority_queue
+void test_priority_queue() {
+    msl::priority_queue<int> pq;
+    
+    assert(pq.empty());
+    assert(pq.size() == 0);
+
+    pq.push(3);
+    pq.push(1);
+    pq.push(4);
+    pq.push(1);
+    pq.push(5);
+
+    assert(!pq.empty());
+    assert(pq.size() == 5);
+    assert(pq.top() == 5);
+
+    pq.pop();
+    assert(pq.top() == 4);
+    pq.pop();
+    assert(pq.top() == 3);
+    
+    // Test iterator constructor
+    int arr[] = {10, 20, 5};
+    
+    msl::priority_queue<int> pq2(arr, arr + 3);
+    assert(pq2.size() == 3);
+    assert(pq2.top() == 20);
+
+    // Test copy constructor
+    msl::priority_queue<int> pq3(pq2);
+    assert(pq3.size() == 3);
+    assert(pq3.top() == 20);
+    pq2.pop();
+    assert(pq3.size() == 3); // pq3 should be independent
+    assert(pq2.size() == 2);
+
+    // Test copy assignment
+    msl::priority_queue<int> pq4;
+    pq4 = pq3;
+    assert(pq4.size() == 3);
+    assert(pq4.top() == 20);
+
+    // Test move constructor
+    msl::priority_queue<int> pq5(msl::move(pq3));
+    assert(pq5.size() == 3);
+    assert(pq5.top() == 20);
+    // pq3 state is unspecified but usually empty for vector
+    // assert(pq3.empty()); // valid for vector implementation
+
+    // Test move assignment
+    msl::priority_queue<int> pq6;
+    pq6 = msl::move(pq5);
+    assert(pq6.size() == 3);
+    assert(pq6.top() == 20);
+
+    std::cout << "priority_queue test passed" << std::endl;
+}
+
+int main() {
+    test_priority_queue();
+    return 0;
+}
